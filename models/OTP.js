4ea072ce8fbad88324098933e645f7db8826afa2
Amit Kumar <15000amitkumar@gmail.com>
2024-03-11 19:13:59 +0530

    Tags and OTP schema

---
+++ models/OTP.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const OTPSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+  },
+  otp: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    expires: 5 * 60,
+  },
+});
+module.exports = model("OTP", OTPSchema);
