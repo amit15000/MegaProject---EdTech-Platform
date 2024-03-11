4b5c5027eb171b01a58800f81b1494cf38289805
Amit Kumar <15000amitkumar@gmail.com>
2024-03-11 19:05:58 +0530

    Rating And Review Schema

---
+++ models/RatingAndReview.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const ratingAndReviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+  rating: {
+    type: Number,
+    require: true,
+  },
+});
+module.exports = mongoose.model("RatingAndReview", ratingAndReviewSchema);
