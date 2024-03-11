a1422106f041b0f4e2f1564b25e1d5008e780757
Amit Kumar <15000amitkumar@gmail.com>
2024-03-11 19:02:03 +0530

    Course Schema

---
+++ models/Course.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  courseName: {
+    type: String,
+    trim: true,
+    require: true,
+  },
+  courseDescription: {
+    type: String,
+    trim: true,
+    require: true,
+  },
+  instructor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+  whatYouWillLearn: {
+    type: String,
+  },
+  courseContent: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Section",
+    },
+  ],
+  ratingAndReview: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "RatingAndReview",
+    },
+  ],
+  price: {
+    type: Number,
+  },
+  thumbnail: {
+    type: String,
+  },
+  tag: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "tag",
+  },
+  studentsEnrolled: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+  ],
+});
+module.exports = mongoose.model("Course", courseSchema);
