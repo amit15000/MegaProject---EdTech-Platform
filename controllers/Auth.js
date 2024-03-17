46e3f8000cd0fd736e12a16bc8bb44ffdcf2ecd3
Amit Kumar <15000amitkumar@gmail.com>
2024-03-17 23:11:48 +0530

    otp middleware

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,3 +1,5 @@
+const { generate } = require("otp-generator");
+const OTP = require("../models/OTP");
 const User = require("../models/Users");
 
 //send OTP
@@ -8,7 +10,43 @@ exports.sendOTP = async (req, res) => {
   try {
     const checkUserExist = await User.findOne({ email });
     if (checkUserExist) {
-      return;
+      return res.json({
+        success: false,
+        message: "User Already Exist",
+      });
     }
-  } catch (error) {}
+
+    //new user--> generate otp
+    let newOTP = generate(6, {
+      specialChars: false,
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+    });
+
+    let sameOTP = OTP.findOne({ otp: newOTP });
+    while (sameOTP) {
+      newOTP = generate(6, {
+        specialChars: false,
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+      });
+      sameOTP = OTP.findOne({ otp: newOTP });
+    }
+    console.log(sameOTP);
+    const otpPayload = { email, sameOTP };
+    //create entry in database
+    const otpBody = await OTP.create(otpPayload);
+    console.log(otpBody);
+    res.status(200).json({
+      success: true,
+      message: "OTP sent successfully",
+      sameOTP,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(200).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
